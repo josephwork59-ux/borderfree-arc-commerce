ac1e5103e32e59b89dfc6869539a00e947e89243
Joseph Kwong <40331296+josephkwong@users.noreply.github.com>
2022-12-11 22:35:30 +0800

    add Navbar development

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+// External import
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// Internal import
+import { Navbar, Footer } from '../components';
 
-export default MyApp
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/194d67c9bd.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
