6f5e6dd460980427a15de18acc180067ff2a9516
Joseph Kwong <40331296+josephkwong@users.noreply.github.com>
2022-12-25 18:21:48 +0800

    add Top Sellers development

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,107 @@
-import Banner from '../components/Banner';
+import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-const Home = () => (
-  <div className="flex justify-center sm:px-4 p-12">
-    <div className="w-full minmd:w-4/5">
-      <Banner
-        name="Discover, collect, and sell extraordinary NFTs"
-        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-center"
-        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-      />
+import { Banner, CreatorCard } from '../components';
+
+import images from '../assets';
+import { makeid } from '../utils/makeId';
+
+const Home = () => {
+  const [hideButtons, setHideButtons] = useState(false);
+  const { theme } = useTheme();
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1000 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      setHideButtons(false);
+    } else {
+      setHideButtons(true);
+    }
+  };
+
+  useEffect(() => {
+    isScollable();
+    window.addEventListener('resize', isScollable);
+
+    return () => {
+      window.removeEventListener('resize', isScollable);
+    };
+  });
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name="Discover, collect, and sell extraordinary NFTs"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-center"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Most Popular Creators</h1>
+
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0x${makeid(3)}...${makeid(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+              {!hideButtons && (
+                <>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-8 bg-nft-red-violet rounded-full minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                  >
+                    <Image
+                      src={images.left}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' ? 'filter invert' : undefined}
+                    />
+                  </div>
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="absolute w-8 h-8 bg-nft-red-violet rounded-full minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                  >
+                    <Image
+                      src={images.right}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' ? 'filter invert' : undefined}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
