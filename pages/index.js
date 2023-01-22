c28245040bff029f5a0f8eeb91d14d7372f1a1a0
Joseph Kwong <40331296+josephkwong@users.noreply.github.com>
2023-01-22 14:45:11 +0800

    Add Most Popular Creators development

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import { Banner, CreatorCard, NFTCard } from '../components';
 
 import images from '../assets';
 import { makeid } from '../utils/makeId';
+import { getCreators } from '../utils/getTopCreators';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const Home = () => {
   const { fetchNFTs } = useContext(NFTContext);
@@ -57,6 +59,8 @@ const Home = () => {
     };
   });
 
+  const topCreators = getCreators(nfts);
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -71,7 +75,16 @@ const Home = () => {
 
           <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {[6, 7, 8, 9, 10].map((i) => (
+              {topCreators.map((creator, i) => (
+                <CreatorCard
+                  key={creator.seller}
+                  rank={i + 1}
+                  creatorImage={images[`creator${i + 1}`]}
+                  creatorName={shortenAddress(creator.seller)}
+                  creatorEths={creator.sum}
+                />
+              ))}
+              {/* {[6, 7, 8, 9, 10].map((i) => (
                 <CreatorCard
                   key={`creator-${i}`}
                   rank={i}
@@ -79,7 +92,7 @@ const Home = () => {
                   creatorName={`0x${makeid(3)}...${makeid(4)}`}
                   creatorEths={10 - i * 0.5}
                 />
-              ))}
+              ))} */}
               {!hideButtons && (
                 <>
                   <div
